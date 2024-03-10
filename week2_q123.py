b6132a4af5b92948ad36a11c87930d537d5d1e1b
Ghqlq <gb2789@nyu.edu>
2024-03-10 17:47:12 -0400

    Create week2_q123.py

---
+++ week2_q123.py
@@ -0,0 +1,31 @@
+
+'''
+The Basics of NumPy and Pandas
+week 2
+'''
+
+#q1
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/Cars93_miss.csv')
+idx = np.arange(0, df.shape[0], step=20)
+col = ['Manufacturer','Model','Type']
+result = df[col].iloc[idx]
+
+print(result)
+
+#q2
+
+df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/Cars93_miss.csv')
+min_price_mean = df['Min.Price'].mean()
+max_price_mean = df['Max.Price'].mean()
+df['Min.Price'].fillna(min_price_mean, inplace=True)
+df['Max.Price'].fillna(max_price_mean, inplace=True)
+print(df[['Min.Price', 'Max.Price']])
+
+
+#q3
+
+df = pd.DataFrame(np.random.randint(10, 40, 60).reshape(-1, 4))
+print(df[df.sum(axis=1) > 100])
+
